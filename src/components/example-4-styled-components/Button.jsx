02d34454afa3252c935cb729c943f67ffd522781
MDeadwiler <mdeadwiler@email.com>
2024-10-17 10:47:34 -0400

    completed-btnSass-styling-react

--- src/components/example-4-styled-components/Button.jsx
+++ src/components/example-4-styled-components/Button.jsx
@@ -1,6 +1,17 @@
+// src/components/example-4-styled-components/Button.jsx
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: #86ba8a;
+  border-radius: 8px;
+  color: #3d3d3d;
+  padding: 15px 50px;
+  font-size: 24pt;
+  width: 300px;
+`;
+
 const Button = ({ buttonText }) => {
-    return <button>{buttonText}</button>;
-  };
-  
-  export default Button;
-  
+  return <StyledButton>{buttonText}</StyledButton>;
+};
+
+export default Button;
--- src/components/example-5-css-modules/Button.jsx
+++ src/components/example-5-css-modules/Button.jsx
@@ -1,6 +1,9 @@
+// src/components/example-5-css-modules/Button.jsx
+
+import styles from './button.module.css';
+
 const Button = ({ buttonText }) => {
-    return <button>{buttonText}</button>;
-  };
-  
-  export default Button;
-  
+  return <button className={styles.btn}>{buttonText}</button>;
+};
+
+export default Button;
