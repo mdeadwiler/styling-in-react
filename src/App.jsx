953e36896b2d68030eea03510a6277d698245a47
MDeadwiler <mdeadwiler@email.com>
2024-10-17 10:16:14 -0400

    added-buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 // src/App.jsx
 import './App.css';
-// Later on, we'll import button components here
+import Button1 from './components/example-1-normal-css/Button';
+import Button2 from './components/example-2-inline-css/Button';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      {
-        // Add all example components:
-        <Button1 buttonText="Example 1" />
-        //...
-      }
+      <Button1 buttonText="Example 1" />
+      <Button2 buttonText="Example 2" />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/example-3-css-in-js/Button.jsx
+++ src/components/example-3-css-in-js/Button.jsx
@@ -1,3 +1,4 @@
+
 const Button = ({ buttonText }) => {
     return <button>{buttonText}</button>;
   };
